e410e5c975db4b8d7eb1f1aeb130e1888b061f82
Bandarupallipavani <pavanibandarupally2@gmail.com>
2026-07-19 22:48:02 +0530

    commit

--- backend/app/main.py
+++ backend/app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://localhost:5173",
+        "https://navigation.vjstartup.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
